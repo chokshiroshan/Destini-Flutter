7e2a36a53b9a81931b912b11bedba1e27f5d65df
Roshan Chokshi <chokshiroshan@gmail.com>
2020-04-09 16:03:29 +0530

    Added App.

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String st;
+  String c1;
+  String c2;
+
+  Story({String storyTitle, String choice1, String choice2}) {
+    st = storyTitle;
+    c1 = choice1;
+    c2 = choice2;
+  }
+}
